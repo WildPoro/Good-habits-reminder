ed60e37c78b202381dfea6d0170837bece82b086
WildPoro <153674208+WildPoro@users.noreply.github.com>
2025-04-19 21:09:06 -0400

    Update and rename habbits scraper to habbits scraper.py

--- habbits scraper.py
+++ habbits scraper.py
@@ -8,7 +8,7 @@ def scrape():
     response.raise_for_status()  # Check if the request was successful
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    habits = [habit.text.strip() for habit in soup.find_all("h3", class_="wp-block-heading")] #the habits have a bunch of extra text i dont need so i am removcin it using class_="wp-block-heading"
+    habits = [habit.text.strip() for habit in soup.find_all("h3", class_="wp-block-heading")] #the habits have a bunch of extra text i dont need so i am removing it using class_="wp-block-heading"
     return habits[:5]
 
 print(scrape())
